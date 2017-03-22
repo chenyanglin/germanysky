ca8de0a4c99892e8ff4af1eeb17a2c156e31cda4
Chenyang Lin <chenyang1205@gmail.com>
2017-03-22 16:25:15 +0800

    0322commit

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,18 @@ module Germanysky
     config.active_record.raise_in_transactional_callbacks = true
     config.web_console.development_only = false
     config.assets.enabled = false
+
+    # Disable Rails's static asset server (Apache or nginx will already do this)
+    config.serve_static_assets = false
+
+    # Compress JavaScripts and CSS
+    config.assets.compress = true
+
+    # Don't fallback to assets pipeline if a precompiled asset is missed
+    config.assets.compile = false
+
+    # Generate digests for assets URLs
+    config.assets.digest = true
+
   end
 end
