a20c6b365a022b61ee14d6492e9bdce8a8641dc5
Chenyang Lin <chenyang1205@gmail.com>
2017-03-22 17:37:59 +0800

    test

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Germanysky
     config.assets.compress = true
 
     # Don't fallback to assets pipeline if a precompiled asset is missed
-    config.assets.compile = false
+    # config.assets.compile = false
 
     # Generate digests for assets URLs
     config.assets.digest = true
